030fae377ff33f68bb455207220ea17c3246de92
essam <essam@34ml.com>
2022-11-09 15:00:46 +0200

    Change the index field vue

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -1,34 +1,42 @@
 <template>
-    <span class="inline-block rounded-full w-2 h-2" :class="seoStatus"></span>
+  <span class="inline-block rounded-full w-2 h-2" :class="seoStatus"></span>
 </template>
 
 <script>
 export default {
-    props: ["resourceName", "field"],
-    computed: {
-        seoStatus() {
-            const field = this.field;
-            const value = this.field.value;
-            if (!field.default_values && value) {
-                if (
-                    ["index, follow", "index, nofollow"].indexOf(
-                        value.follow_type
-                    ) >= 0
-                ) {
-                    let hasTitleForAnyLocale = false;
-                    for (const locale in value.title) {
-                        if (value.title[locale] && value.title[locale].trim() !== ''){
-                            hasTitleForAnyLocale = true;
-                        }
-                    }
-                    if (hasTitleForAnyLocale) {
-                        return "bg-success";
-                    }
-                    return "bg-warning";
-                }
+  props: ["resourceName", "field"],
+  computed: {
+    seoStatus() {
+      const field = this.field;
+      const value = this.field.value;
+      if (!field.default_values && value) {
+        if (
+            ["index, follow", "index, nofollow"].indexOf(
+                value.follow_type
+            ) >= 0
+        ) {
+          let hasTitleForAnyLocale = false;
+          let hasDescriptionForAnyLocale = false;
+          let hasKeywordsForAnyLocale = false;
+          for (const locale in value.title) {
+            if (value.title[locale] && value.title[locale].trim() !== ''){
+              hasTitleForAnyLocale = true;
             }
-            return "bg-danger";
+            if (value.description[locale] && value.description[locale].trim() !== ''){
+              hasDescriptionForAnyLocale = true;
+            }
+            if (value.keywords[locale] && value.keywords[locale].trim() !== ''){
+              hasKeywordsForAnyLocale = true;
+            }
+          }
+          if (hasTitleForAnyLocale && hasDescriptionForAnyLocale && hasKeywordsForAnyLocale) {
+            return "bg-success";
+          }
+          return "bg-warning";
         }
+      }
+      return "bg-danger";
     }
+  }
 };
 </script>
