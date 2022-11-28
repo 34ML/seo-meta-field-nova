056d10fad9521e7ce1444b77feb64a4af40ecf3c
essam <essam@34ml.com>
2022-11-28 19:18:18 +0200

    Change the index field vue colors

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -34,7 +34,7 @@ export default {
         return "bg-success";
       }
 
-      if (hasTitleForAnyLocale || hasDescriptionForAnyLocale || hasKeywordsForAnyLocale) {
+      if (!hasTitleForAnyLocale || !hasDescriptionForAnyLocale || !hasKeywordsForAnyLocale) {
         return "bg-warning";
       }
 
