4d2ae206a3a72ed98336200757e50e2fe1de738c
essam <essam@34ml.com>
2022-11-28 19:07:34 +0200

    Change the index field vue colors

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -9,41 +9,38 @@ export default {
     seoStatus() {
       const field = this.field;
       const value = this.field.value;
-      if (!field.default_values && value) {
-        let hasTitleForAnyLocale = false;
-        let hasDescriptionForAnyLocale = false;
-        let hasKeywordsForAnyLocale = false;
-        let hasFollow = false;
+      let hasTitleForAnyLocale = false;
+      let hasDescriptionForAnyLocale = false;
+      let hasKeywordsForAnyLocale = false;
+      let hasFollow = false;
 
-        for (const locale in value.title) {
-          if (value.title[locale] && value.title[locale].trim() !== ''){
-            hasTitleForAnyLocale = true;
-          }
-          if (value.description[locale] && value.description[locale].trim() !== ''){
-            hasDescriptionForAnyLocale = true;
-          }
-          if (value.keywords[locale] && value.keywords[locale].trim() !== ''){
-            hasKeywordsForAnyLocale = true;
-          }
+      for (const locale in value.title) {
+        if (value.title[locale] && value.title[locale].trim() !== ''){
+          hasTitleForAnyLocale = true;
         }
-
-        if(["index, follow", "index, nofollow"].indexOf(value.follow_type) >= 0) {
-          hasFollow = true;
+        if (value.description[locale] && value.description[locale].trim() !== ''){
+          hasDescriptionForAnyLocale = true;
         }
-
-        if (hasTitleForAnyLocale && hasDescriptionForAnyLocale && hasKeywordsForAnyLocale && hasFollow) {
-          return "bg-success";
-        }
-
-        if (hasTitleForAnyLocale || hasDescriptionForAnyLocale || hasKeywordsForAnyLocale) {
-          return "bg-warning";
-        }
-
-        if(!hasTitleForAnyLocale && !hasDescriptionForAnyLocale && !hasKeywordsForAnyLocale && !hasFollow) {
-          return "bg-danger";
+        if (value.keywords[locale] && value.keywords[locale].trim() !== ''){
+          hasKeywordsForAnyLocale = true;
         }
       }
-      return "bg-danger";
+
+      if(["index, follow", "index, nofollow"].indexOf(value.follow_type) >= 0) {
+        hasFollow = true;
+      }
+
+      if (hasTitleForAnyLocale && hasDescriptionForAnyLocale && hasKeywordsForAnyLocale && hasFollow) {
+        return "bg-success";
+      }
+
+      if (hasTitleForAnyLocale || hasDescriptionForAnyLocale || hasKeywordsForAnyLocale) {
+        return "bg-warning";
+      }
+
+      if(!hasTitleForAnyLocale && !hasDescriptionForAnyLocale && !hasKeywordsForAnyLocale && !hasFollow) {
+        return "bg-danger";
+      }
     }
   }
 };
